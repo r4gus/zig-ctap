9b0a430e0c55cadf67517b5a7f61941f145cab39
David Pierre Sugar <david@thesugar.de>
2022-08-29 23:27:55 +0200

    made changes to uart

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,28 @@ const status_led_pin = micro.Pin("PA14");
 const user_switch_pin = micro.Pin("PA15");
 
 pub fn main() void {
+    micro.chip.nvmctrlInit();
+
     const status_led = micro.Gpio(status_led_pin, .{
         .mode = .output,
         .initial_state = .low,
     });
     status_led.init();
+
+    var uart = micro.Uart(5, .{ .tx = null, .rx = null }).init(.{
+        //.baud_rate = 63019, // 65536 * ( 1 - 16 * (115200 / 48_000_000)), p. 830
+        .baud_rate = 65326, // 65536 * ( 1 - 16 * (115200 / 48_000_000)), p. 830
+        .stop_bits = .one,
+        .parity = null,
+        .data_bits = .eight,
+    }) catch |err| {
+        blinkError(status_led, err);
+
+        micro.hang();
+    };
+
+    var out = uart.writer();
+    _ = out;
 
     //const user_switch = micro.Gpio(user_switch_pin, .{.mode = .input});
     //user_switch.init();
@@ -17,6 +34,7 @@ pub fn main() void {
     while (true) {
         busyloop();
         status_led.toggle();
+        uart.internal.tx('X');
 
         //if (micro.chip.gpio.read(user_switch_pin.source_pin) == .low) {
         //    status_led.write(.low);
@@ -39,14 +57,14 @@ fn blinkError(led: anytype, err: micro.uart.InitError) void {
 
     while (blinks > 0) : (blinks -= 1) {
         led.toggle();
-        micro.debug.busySleep(500_000_000);
+        micro.debug.busySleep(1_000_000);
         led.toggle();
-        micro.debug.busySleep(500_000_000);
+        micro.debug.busySleep(1_000_000);
     }
 }
 
 fn busyloop() void {
-    const limit = 15_000_000;
+    const limit = 5_000_000;
 
     var i: u32 = 0;
     while (i < limit) : (i += 1) {
