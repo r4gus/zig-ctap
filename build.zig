e1206e7dd34c14f79777ec433b280a3b3ebbc7f6
David Pierre Sugar <david@thesugar.de>
2022-10-10 23:48:08 +0200

    compiles but doesnt work

--- build.zig
+++ build.zig
@@ -1,19 +1,152 @@
 const std = @import("std");
 const microzig = @import("libs/microzig/src/main.zig");
 
+fn compileTinyUsb(self: *std.build.Step) !void {
+    _ = self;
+}
+
 pub fn build(b: *std.build.Builder) void {
     const backing = .{
         .chip = microzig.chips.atsame51j20a,
     };
 
+    // ########################################################################
+    // tinyusb
+    // ########################################################################
+
+    const tinyusb_path = "libs/tinyusb/";
+    const flags = [_][]const u8{
+        "-ggdb",
+        "-fdata-sections",
+        "-ffunction-sections",
+        //"-fsingle-precision-constant",
+        "-fno-strict-aliasing",
+        "-Wall",
+        "-Wextra",
+        "-Werror",
+        "-Wfatal-errors",
+        "-Wdouble-promotion",
+        "-Wstrict-prototypes",
+        "-Wstrict-overflow",
+        "-Werror-implicit-function-declaration",
+        "-Wfloat-equal",
+        "-Wundef",
+        "-Wshadow",
+        "-Wwrite-strings",
+        "-Wsign-compare",
+        "-Wmissing-format-attribute",
+        "-Wunreachable-code",
+        "-Wcast-align",
+        "-Wcast-function-type",
+        //"-Wcast-qual",
+        "-Wnull-dereference",
+        "-Wuninitialized",
+        "-Wunused",
+        "-Wredundant-decls",
+        // chip
+        "-mabi=aapcs",
+        "-mlong-calls",
+        "-mfloat-abi=hard",
+    };
+    const obj = [_][]const u8{
+        // tinyusb core
+        tinyusb_path ++ "src/tusb.c",
+        tinyusb_path ++ "src/common/tusb_fifo.c",
+        tinyusb_path ++ "src/device/usbd.c",
+        tinyusb_path ++ "src/device/usbd_control.c",
+        tinyusb_path ++ "src/class/audio/audio_device.c",
+        tinyusb_path ++ "src/class/cdc/cdc_device.c",
+        tinyusb_path ++ "src/class/dfu/dfu_device.c",
+        tinyusb_path ++ "src/class/dfu/dfu_rt_device.c",
+        tinyusb_path ++ "src/class/hid/hid_device.c",
+        tinyusb_path ++ "src/class/midi/midi_device.c",
+        tinyusb_path ++ "src/class/msc/msc_device.c",
+        tinyusb_path ++ "src/class/net/ecm_rndis_device.c",
+        tinyusb_path ++ "src/class/net/ncm_device.c",
+        tinyusb_path ++ "src/class/usbtmc/usbtmc_device.c",
+        tinyusb_path ++ "src/class/video/video_device.c",
+        tinyusb_path ++ "src/class/vendor/vendor_device.c",
+        // app
+        "c_src/usb_descriptors.c",
+        "c_src/same51curiositynano.c",
+        // chip specific
+        tinyusb_path ++ "src/portable/microchip/samd/dcd_samd.c",
+        tinyusb_path ++ "hw/mcu/microchip/same51/gcc/gcc/startup_same51.c",
+        tinyusb_path ++ "hw/mcu/microchip/same51/gcc/system_same51.c",
+        tinyusb_path ++ "hw/mcu/microchip/same51/hal/utils/src/utils_syscalls.c",
+        // board specific
+    };
+
+    var tinyusb = b.addStaticLibrary("tinyusb", null);
+    tinyusb.addCSourceFiles(&obj, &flags);
+    tinyusb.addIncludePath(tinyusb_path ++ "hw");
+    tinyusb.addIncludePath(tinyusb_path ++ "src");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/audio");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/bth");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/cdc");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/dfu");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/hid");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/midi");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/msc");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/net");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/usbtmc");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/vendor");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/class/video");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/common");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/device");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/host");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/osal");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/portable/chipidea/ci_hs");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/portable/ehci");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/portable/mentor/musb");
+    tinyusb.addIncludePath(tinyusb_path ++ "src/portable/microchip/samx7x");
+    // main
+    tinyusb.addIncludePath("c_src");
+    // system
+    tinyusb.addIncludePath("/usr/include");
+    tinyusb.addIncludePath("/usr/local/include");
+    // chip specific
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/config");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/include");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/hal/include");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/hal/utils/include");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/hpl/port");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/hri");
+    tinyusb.addIncludePath(tinyusb_path ++ "hw/mcu/microchip/same51/CMSIS/Include");
+    // board specific
+    tinyusb.addIncludePath(tinyusb_path ++ "bsp");
+    tinyusb.defineCMacro("__SAME51J20A__", "1");
+    tinyusb.defineCMacro("CONF_CPU_FREQUENCY", "48000000");
+    tinyusb.defineCMacro("CFG_TUSB_MCU", "OPT_MCU_SAME5X");
+    tinyusb.defineCMacro("BOARD_NAME", "\"Microchip SAM E51 Curiosity Nano\"");
+    tinyusb.setTarget(std.zig.CrossTarget{
+        .cpu_arch = .thumb,
+        .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m4 },
+        .os_tag = .freestanding,
+        .abi = .eabihf,
+    });
+    tinyusb.install();
+
+    // ########################################################################
+    // main
+    // ########################################################################
+
     var exe = microzig.addEmbeddedExecutable(b, "zig-ctap", "src/main.zig", backing, .{
         // optional slice of packages that can be imported into your app:b
         // .packages = &my_packages,
     });
+    exe.inner.step.dependOn(&tinyusb.step);
 
     exe.setBuildMode(.ReleaseSmall);
     exe.inner.strip = true;
     exe.addPackagePath("zbor", "libs/zbor/src/main.zig");
+    //exe.inner.addLibraryPath("zig-out/lib/libtinyusb.a");
+    exe.inner.linkLibrary(tinyusb);
+    //exe.inner.linkSystemLibrary("m");
+    //exe.inner.linkSystemLibrary("nosys");
+    //exe.inner.linkSystemLibrary("gcc");
+    exe.inner.addObjectFile("/usr/arm-none-eabi/lib/thumb/v7e-m+fp/hard/libc.a");
     exe.install();
 
     // ########################################################################
--- src/main.zig
+++ src/main.zig
@@ -5,54 +5,59 @@ const cbor = @import("zbor");
 const status_led_pin = micro.Pin("PA14");
 const user_switch_pin = micro.Pin("PA15");
 
-pub fn main() !void {
-    micro.chip.nvmctrlInit();
-    micro.chip.enableTrng();
-    micro.chip.crypto.public_key.init();
-
-    const status_led = micro.Gpio(status_led_pin, .{
-        .mode = .output,
-        .initial_state = .low,
-    });
-    status_led.init();
-
-    var uart = micro.Uart(5, .{ .tx = null, .rx = null }).init(.{
-        .baud_rate = 115200,
-        .stop_bits = .one,
-        .parity = null,
-        .data_bits = .eight,
-    }) catch |err| {
-        blinkError(status_led, err);
-
-        micro.hang();
-    };
-
-    var out = uart.writer();
-
-    micro.chip.gpio.setInputPullUp(user_switch_pin.source_pin);
-
-    while (true) {
-        // Fixed buffer allocator won't reuse freed memory
-        // so no point in keeping it around.
-        var buffer: [512]u8 = undefined;
-        var fba = std.heap.FixedBufferAllocator.init(&buffer);
-        const allocator = fba.allocator();
-
-        busyloop();
-        status_led.toggle();
-
-        const r: u32 = micro.chip.crypto.random.getWord();
-
-        var di = try cbor.DataItem.map(allocator, &.{
-            cbor.Pair.new(try cbor.DataItem.text(allocator, "random"), cbor.DataItem.int(r)),
-        });
-        defer di.deinit(allocator);
-
-        //try cbor.encode(out, &di);
-        try out.print("random: {d}", .{r});
-        try out.writeAll("\n\r");
-    }
-}
+//pub fn main() !void {
+//    micro.chip.nvmctrlInit();
+//    micro.chip.enableTrng();
+//
+//    const status_led = micro.Gpio(status_led_pin, .{
+//        .mode = .output,
+//        .initial_state = .low,
+//    });
+//    status_led.init();
+//
+//    var uart = micro.Uart(5, .{ .tx = null, .rx = null }).init(.{
+//        .baud_rate = 115200,
+//        .stop_bits = .one,
+//        .parity = null,
+//        .data_bits = .eight,
+//    }) catch |err| {
+//        blinkError(status_led, err);
+//
+//        micro.hang();
+//    };
+//
+//    var out = uart.writer();
+//
+//    micro.chip.gpio.setInputPullUp(user_switch_pin.source_pin);
+//
+//    while (true) {
+//        // Fixed buffer allocator won't reuse freed memory
+//        // so no point in keeping it around.
+//        var buffer: [512]u8 = undefined;
+//        var fba = std.heap.FixedBufferAllocator.init(&buffer);
+//        const allocator = fba.allocator();
+//
+//        busyloop();
+//        status_led.toggle();
+//
+//        const r: u32 = micro.chip.crypto.random.getWord();
+//
+//        var x: [12]u8 = undefined;
+//        micro.chip.crypto.random.getBlock(&x);
+//
+//        var di = try cbor.DataItem.map(allocator, &.{
+//            cbor.Pair.new(try cbor.DataItem.text(allocator, "random"), cbor.DataItem.int(r)),
+//        });
+//        defer di.deinit(allocator);
+//
+//        //try cbor.encode(out, &di);
+//        //try out.print("random: {d}", .{r});
+//        for (x) |value| {
+//            try out.print("{x}", .{value});
+//        }
+//        try out.writeAll("\n\r");
+//    }
+//}
 
 fn blinkError(led: anytype, err: micro.uart.InitError) void {
     var blinks: u3 =
@@ -78,4 +83,149 @@ fn busyloop() void {
     while (i < limit) : (i += 1) {
         @import("std").mem.doNotOptimizeAway(i);
     }
+}
+
+//--------------------------------------------------------------------+
+// Extern
+//--------------------------------------------------------------------+
+
+// defined in tinyusb/src/class/hid/hid_device.h
+extern fn tud_hid_n_report(instance: u8, report_id: u8, report: [*]const u8, len: u16) bool;
+extern fn board_millis() u32;
+extern fn board_led_write(state: bool) void;
+extern fn board_init() void;
+extern fn tud_init(rhport: u8) bool;
+/// Task function should be called in main/rtos loop, extended version of tudTask.
+/// - timeout_ms: milliseconds to wait, zero = no wait, 0xffffffff = wait forever
+/// - in_isr: if function is called in ISR
+extern fn tud_task_ext(timeout_ms: u32, in_isr: bool) void;
+
+const HidReportType = enum(c_int) {
+    // defined in tinyusb/src/class/hid/hid.h
+    invalid = 0,
+    input,
+    output,
+    feature,
+};
+
+//--------------------------------------------------------------------+
+// Wrapper
+//--------------------------------------------------------------------+
+
+fn tudHidReport(report_id: u8, report: []const u8) bool {
+    if (report.len > 65535) {
+        return false;
+    }
+
+    // Note: the narrowing int cast is safe because of the check above.
+    return tud_hid_n_report(0, report_id, report.ptr, @intCast(u16, report.len));
+}
+
+// Task function should be called in main loop.
+fn tudTask() void {
+    // UINT32_MAX
+    tud_task_ext(0xffffffff, false);
+}
+
+//--------------------------------------------------------------------+
+// Main
+//--------------------------------------------------------------------+
+
+pub fn main() void {
+    board_init();
+
+    // init device stack on configured roothub port.
+    _ = tud_init(0);
+
+    while (true) {
+        tudTask(); // tinyusb device task
+        led_blinking_task();
+    }
+}
+
+//--------------------------------------------------------------------+
+// MACRO CONSTANT TYPEDEF PROTYPES
+//--------------------------------------------------------------------+
+
+/// Blink pattern
+/// - 250 ms    : device not mounted
+/// - 1000 ms   : device mounted
+/// - 2500 ms   : device is suspended
+const Blink = enum(u32) {
+    not_mounted = 250,
+    mounted = 1000,
+    suspended = 2500,
+};
+
+var blink_interval_ms: u32 = @enumToInt(Blink.not_mounted);
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+/// Invoked when device is mounted.
+export fn tud_mount_cb() void {
+    blink_interval_ms = @enumToInt(Blink.mounted);
+}
+
+// Invoked when device is unmounted.
+export fn tud_umount_cb() void {
+    blink_interval_ms = @enumToInt(Blink.not_mounted);
+}
+
+/// Invoked when usb bus is suspended
+/// remote_wakeup_en : if host allow us  to perform remote wakeup
+/// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+export fn tud_suspend_cb(remote_wakeup_en: bool) void {
+    _ = remote_wakeup_en;
+    blink_interval_ms = @enumToInt(Blink.suspended);
+}
+
+// Invoked when usb bus is resumed.
+export fn tud_resume_cb() void {
+    blink_interval_ms = @enumToInt(Blink.mounted);
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+/// Invoked when received SET_REPORT control request or
+/// received data on OUT endpoint ( Report ID = 0, Type = 0 ).
+export fn tud_hid_set_report_cb(itf: u8, report_id: u8, report_type: HidReportType, buffer: [*]u8, bufsize: u16) void {
+    _ = itf;
+    _ = report_id;
+    _ = report_type;
+
+    _ = tudHidReport(0, buffer[0..bufsize]);
+}
+
+// Invoked when received GET_REPORT control request.
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request.
+export fn tud_hid_get_report_cb(itf: u8, report_id: u8, report_type: HidReportType, buffer: [*]u8, reqlen: u16) u16 {
+    _ = itf;
+    _ = report_id;
+    _ = report_type;
+    _ = buffer;
+    _ = reqlen;
+
+    return 0;
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+
+export fn led_blinking_task() void {
+    const S = struct {
+        var start_ms: u32 = 0;
+        var led_state: bool = false;
+    };
+
+    if (board_millis() - S.start_ms < blink_interval_ms) return;
+    S.start_ms += blink_interval_ms;
+
+    board_led_write(S.led_state);
+    S.led_state = !S.led_state;
 }
